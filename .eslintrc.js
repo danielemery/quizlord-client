f5f533f437d89813a3663ad7e11ca0ff6947f7e4
Daniel Emery <daniel.emery@abusix.com>
2022-07-11 14:24:07 +0200

    #10 Disable node rules and exclude remaining config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,19 @@
 module.exports = {
   env: {
-    node: true,
+    node: false,
     es6: true,
     mocha: true,
     browser: true,
   },
   extends: ['@dtdot/eslint-config/base'],
-  ignorePatterns: ['node_modules', 'dist', '.prettierrc.js', 'public', 'env.schema.js'],
+  ignorePatterns: [
+    'node_modules',
+    'dist',
+    '.prettierrc.js',
+    '.eslintrc.js',
+    'public',
+    'env.schema.js',
+    'postcss.config.js',
+    'tailwind.config.js',
+  ],
 };
