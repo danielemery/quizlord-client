32439160869ca17c93aa99c23c63bb54e28d91a0
Daniel Emery <danielremery@gmail.com>
2023-07-06 08:45:40 +0200

    #14 Include api version from zustand in footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,4 +1,7 @@
+import { useVersionStore } from './services/version';
+
 export default function Footer() {
+  const apiVersion = useVersionStore((state) => state.version);
   return (
     <footer class='fixed bottom-0 left-0 right-0 bg-slate-500'>
       <div class='w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between'>
@@ -6,7 +9,7 @@ export default function Footer() {
         <ul class='flex flex-wrap items-center mt-3 text-sm font-medium text-white sm:mt-0 space-x-2'>
           <li>
             <a href='https://github.com/danielemery/quizlord-api/releases' class='hover:underline' target='_blank'>
-              API [vX.X.X]
+              API [{apiVersion ?? 'X.X.X'}]
             </a>
           </li>
           <li>
