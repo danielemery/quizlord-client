4fd974502e636b3fae62e57caef194513d572eaa
Daniel Emery <danielremery@gmail.com>
2023-10-07 22:45:59 +0200

    #38 Fix linting issue

--- src/app.tsx
+++ src/app.tsx
@@ -5,10 +5,10 @@ import EnterQuizResults from './EnterQuizResults';
 import Footer from './Footer';
 import NavigationBar from './NavigationBar';
 import QuizDetails from './QuizDetails';
-import QuizList from './pages/list/QuizList';
 import { useQuizlord } from './QuizlordProvider';
 import Button from './components/Button';
 import Loader from './components/Loader';
+import QuizList from './pages/list/QuizList';
 
 export function App() {
   const { isAuthenticated, user, logout, loginWithRedirect, isLoading } = useQuizlord();
