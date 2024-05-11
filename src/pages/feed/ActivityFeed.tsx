7e952805353f3ca1d0500fa3656399c791a15075
Daniel Emery <danielremery@gmail.com>
2024-05-11 12:58:31 +0200

    #46 Simplify the design of the activity feed

--- src/pages/feed/ActivityFeed.tsx
+++ src/pages/feed/ActivityFeed.tsx
@@ -1,29 +1,28 @@
-import Button from '../../components/Button';
 import Loader from '../../components/Loader';
-import { formatDate, userIdentifier } from '../../helpers';
+import { Table } from '../../components/Table';
+import { formatDateTime, userIdentifier } from '../../helpers';
 import { useActivityFeed } from '../../hooks/useActivityFeed.hook';
 
 export default function ActivityFeed() {
-  const { data, loading, refetch } = useActivityFeed();
+  const { data, loading } = useActivityFeed();
 
   if (loading || !data) {
     return <Loader message='Loading recent activities...' />;
   }
 
   return (
-    <ul>
-      {data?.activityFeed.map((activity) => (
-        <li className='mt-4'>
-          <strong>{formatDate(activity.date)}:</strong> {activity.text}{' '}
-          {activity.users.map((u) => userIdentifier(u)).join(', ')}
-        </li>
-      ))}
-      {/* // TODO Remove or replace */}
-      <li>
-        <Button onClick={() => refetch()} className='mt-4'>
-          Test
-        </Button>
-      </li>
-    </ul>
+    <Table>
+      <Table.Body>
+        {data?.activityFeed.map((activity) => (
+          <Table.Row>
+            <Table.Cell>{activity.users.map((u) => userIdentifier(u)).join(', ')}</Table.Cell>
+            <Table.Cell>{activity.text}</Table.Cell>
+            <Table.Cell>
+              <i>{formatDateTime(activity.date)}</i>
+            </Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table>
   );
 }
